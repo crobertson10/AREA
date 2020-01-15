2a1c875b14894af546bf6fbf29caa25c3a9d265b
tal <rodolphe.tal@epitech.eu>
2020-01-15 15:29:28 +0100

    [+] home page start

--- web/src/App.js
+++ web/src/App.js
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import { 
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import Home from "./Pages/Home";
+import { Switch, Route, Link } from "react-router-dom";
+import "./App.css";
 
 function App() {
   return (
     <Switch>
       <Route path="/">
-
-      </Route>
-      <Route path="/signup">
-        
-      </Route>
-      <Route path="/signin">
-        
-      </Route>
-      <Route path="/home">
-        
+        <Home />
       </Route>
     </Switch>
   );

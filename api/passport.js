9f0b68695e7d80ecefcac6229e44556350566269
Tom <tom.le-roux@epitech.eu>
2020-02-11 14:10:13 +0100

    [-] delete azure srategy

--- api/passport.js
+++ api/passport.js
@@ -2,12 +2,9 @@ const passport = require('passport');
 
 const githubStrategy = require('./strategy/githubStrategy');
 const slackStrategy = require('./strategy/slackStrategy');
-const azureStrategy = require('./strategy/azureStrategy');
 
 passport.use('github', githubStrategy);
 
 passport.use('slack', slackStrategy);
-
-passport.use('azure', azureStrategy);
 
 module.exports = passport;

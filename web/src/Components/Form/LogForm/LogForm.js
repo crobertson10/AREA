e3039d95ab6eecf9f5945d9c0994a809043b7e70
rod <rodolphe.tal@epitech.eu>
2020-02-18 12:19:02 +0100

    [+] private route + new design

--- web/src/Components/Form/LogForm/LogForm.js
+++ web/src/Components/Form/LogForm/LogForm.js
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
+import { Redirect } from "react-router-dom";
 import axios from "axios";
-import { Button, Form } from "react-bootstrap";
+import { Button, Form, Alert } from "react-bootstrap";
+import { login } from "../../Routes/utils";
 import "./LogForm.css";
 
 function LogForm(props) {
   const [email, setEmail] = useState("");
   const [pass, setPass] = useState("");
-
+  const [redir, setRedir] = useState(0);
   const mail = event => {
     setEmail(event.target.value);
   };
@@ -22,13 +24,18 @@ function LogForm(props) {
       })
       .then(function(response) {
         console.log(response);
+        login(response.data.authToken);
+        setRedir(1);
       })
       .catch(function(error) {
         console.log(error);
+        setRedir(2);
       });
   };
   return (
     <Form>
+      {redir === 1 && <Redirect to="/dashboard" />}
+      {redir === 2 && <Alert variant="danger">Wrong Email or Password</Alert>}
       <Form.Group>
         <Form.Label className="Typo">Email address</Form.Label>
         <Form.Control type="email" placeholder="Enter email" onChange={mail} />
@@ -37,7 +44,9 @@ function LogForm(props) {
         <Form.Label className="Typo">Password</Form.Label>
         <Form.Control type="password" placeholder="Password" onChange={passW} />
       </Form.Group>
-      <Button onClick={submit}>Submit</Button>
+      <Button className="LogFormButton" onClick={submit}>
+        Submit
+      </Button>
     </Form>
   );
 }
--- web/src/Components/Header/Header.js
+++ web/src/Components/Header/Header.js
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import Logo from "Assets/logo.png";
+import LogOutModal from "Components/Modal/LogOutModal/LogOutModal";
 import "./Header.css";
 
 function Header(props) {
+  const [show, setShow] = useState(false);
+  const [nshow, nsetShow] = useState(false);
   return (
     <div className="Header">
       <img className="Logo" src={Logo} alt=""></img>
-      <button>Create a Zap</button>
-      <button>Connect an account</button>
+      <div className="RightItem">
+        <button className="Button" onClick={() => setShow(true)}>
+          Create a Zap
+        </button>
+        <button className="Button" onClick={() => nsetShow(true)}>
+          Log Out
+        </button>
+        <LogOutModal show={nshow} onHide={() => nsetShow(false)} />
+      </div>
     </div>
   );
 }

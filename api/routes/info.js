dbc025267f9fe7eda672ee934f234a4f489cdd28
Tom <tom.le-roux@epitech.eu>
2020-03-10 15:27:29 +0100

    [+] delete logs and comms

--- api/routes/info.js
+++ api/routes/info.js
@@ -6,41 +6,35 @@ const Action = require("../models/actionSchema");
 const Reaction = require("../models/reactionSchema");
 
 router.post("/services", verify, (req, res) => {
-  console.log(req.body);
   const authToken = req.body.authToken;
-  console.log(authToken);
   const verified = jwt.verify(authToken, process.env.TOKEN_SECRET);
-  console.log(verified);
   Registered.find({ userId: verified._id }, function(err, docs) {
     if (err) {
-      console.log(err);
       return res.status(400).send(err);
     }
-    console.log(docs);
-    
     res.send(docs);
   });
 });
 
-router.get("/actions", verify, (req, res) => {
+router.post("/actions", verify, (req, res) => {
   Action.find({ service: req.body.service }, function(err, docs) {
     if (err) {
-      // console.log(err);
-      // return res.status(400).send(err);
+      console.log(err);
+      return res.status(400).send(err);
     }
-    // res.send(docs);
+    res.send(docs);
   });
 });
 
-router.get("/reactions", verify, (req, res) => {
+router.post("/reactions", verify, (req, res) => {
   const authToken = req.body.authToken;
   const verified = jwt.verify(authToken, process.env.TOKEN_SECRET);
   Reaction.find({ service: req.body.service }, function(err, docs) {
     if (err) {
-      // console.log(err);
-      // return res.status(400).send(err);
+      console.log(err);
+      return res.status(400).send(err);
     }
-    // res.send(docs);
+    res.send(docs);
   });
 });
 

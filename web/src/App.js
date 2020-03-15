e5c21ecfcef1f50aadac5453d72c86b3f3b27a6a
rod <rodolphe.tal@epitech.eu>
2020-03-15 15:54:39 +0100

    [+] mobile view

--- web/src/App.js
+++ web/src/App.js
@@ -4,17 +4,28 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Login from "Pages/Login/Login";
 import PrivateRoute from "./Components/Routes/PrivateRoutes";
 import Dashboard from "Pages/Dashboard/Dashboard";
+import {
+  BrowserView,
+  MobileView,
+  isBrowser,
+  isMobile
+} from "react-device-detect";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Switch>
-          <PrivateRoute exact component={Dashboard} path="/dashboard" />
-          <Route exact component={Login} path="/" />
-        </Switch>
-      </BrowserRouter>
+      <BrowserView>
+        <BrowserRouter>
+          <Switch>
+            <PrivateRoute exact component={Dashboard} path="/dashboard" />
+            <Route exact component={Login} path="/" />
+          </Switch>
+        </BrowserRouter>
+      </BrowserView>
+      <MobileView>
+        <h1>coucou</h1>
+      </MobileView>
     </div>
   );
 }
